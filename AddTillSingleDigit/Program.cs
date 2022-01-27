c847f51348f7ee94389e8f8e04038cf3e614a5d6
moe-mohammed <73504097+moe-mohammed@users.noreply.github.com>
2022-01-26 23:28:26 -0800

    First commit

---
+++ AddTillSingleDigit/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddTillSingleDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = 11;
+            int singleDigitResult = AddTillOneDigit(power);
+            Console.WriteLine(singleDigitResult);
+            Console.ReadLine();
+        }
+
+        public static int AddTillOneDigit(int power)
+        {
+            int total = (int)Math.Pow(2, power);
+            string finalNumberString = "";
+
+            while (finalNumberString.Length != 1)
+            {
+                char[] totalCharArray = total.ToString().ToCharArray();
+                List<int> numbersList = new List<int>() { };
+
+                foreach (char character in totalCharArray)
+                {
+                    int number = (int)Char.GetNumericValue(character);
+                    numbersList.Add(number);
+                }
+
+                int numbersTotal = 0;
+                foreach (int number in numbersList)
+                {
+                    numbersTotal += number;
+                }
+
+                total = numbersTotal;
+                finalNumberString = total.ToString();
+            }
+
+            return total;
+        }
+    }
+}
